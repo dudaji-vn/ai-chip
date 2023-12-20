e79ec1a18ace0b194cd334fcf2813b17024be76b
VinhTieng <vinhtieng123@gmail.com>
2023-12-20 08:39:27 +0700

    Fix font error when build

--- src/components/shared/Status/index.tsx
+++ src/components/shared/Status/index.tsx
@@ -12,6 +12,6 @@ export default function Status({ status, className }: Props) {
     if(!status) return null;
 
     return (
-        <span className={twMerge('text-green-400 after:bg-green-400 relative pl-4 after:content-[""] after:absolute after:top-[50%] after:left-0 after:translate-y-[-50%] after:w-2 after:h-2 after:rounded-full font-light flex items-center justify-center text-sm', classesStatus[status], className)}>{status}</span>
+        <span className={twMerge('text-green-400 after:bg-green-400 relative pl-4 after:content-[""] after:absolute after:top-[50%] after:left-0 after:translate-y-[-50%] after:w-2 after:h-2 after:rounded-full font-light flex items-center text-sm', classesStatus[status], className)}>{status}</span>
     )
 }
